bdf832fbd1402627933f6f941600c9388d711194
Dur-hy <76396111+Borrachamano@users.noreply.github.com>
2021-05-22 11:57:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,34 @@ def msg(message):
 	bot.send_message(message.chat.id, f'Ola, {message.from_user.first_name}!\n Sou um bot totalmente desenvolvido em python e criado pelo <seu nome>.')
 
 
+
+@bot.message_handler(commands=['game'])
+def jg(message):
+	m = (message.text).replace('/game','').replace(' ', '')
+	sorteado = str(randint(0, 10))
+	try:
+		if m == sorteado:
+			result = f'''
+>>> Partida inicializada <<<
+
+Bot: {sorteado}
+{message.from_user.first_name}:  {m}
+
+Vencedor: {message.from_user.first_name}'''
+		else:
+			result = f'''
+>>> Partida inicializada <<<
+
+Bot: {sorteado}
+{message.from_user.first_name}:  {m}
+
+Vencedor: Bot hehehehe!''' 
+		bot.send_message(message.chat.id, result)
+	except:
+		print('Alguem invocou um erro mestre ;-;')
+
+
+
+
 bot.polling(none_stop=True)
 bot.polling(interval=3)
